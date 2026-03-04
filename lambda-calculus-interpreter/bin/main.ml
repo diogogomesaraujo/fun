4990c8baa9b7ad1c6ea1fcbc1cf65d807a9848d1
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-03-04 01:00:37 +0000

    refactor

--- lambda-calculus-interpreter/bin/main.ml
+++ lambda-calculus-interpreter/bin/main.ml
@@ -5,4 +5,4 @@ let () =
   let fact_5 = Let ("fact", Fix (Lambda ("g", Lambda ("x", IfZero (Variable "x", Constant 1, Multiplication (Variable "x", Application (Variable "g", Subtraction (Variable "x", Constant 1))))))), Application (Variable "fact", Constant 5)) in
   match eval fact_5 with
   | Constant x -> Printf.printf "%d\n" x
-  | _ -> failwith "error"
+  | _ -> failwith "could not reach a constant value"
--- lambda-calculus-interpreter/lib/eval.ml
+++ lambda-calculus-interpreter/lib/eval.ml
@@ -6,31 +6,31 @@ let rec eval e =
   | Constant n -> Constant n
   | Variable _ -> failwith "free variable occurence"
   | Addition (e1, e2) ->
-      (match (eval e1,eval e2) with
-    |   (Constant e1, Constant e2) -> Constant (e1 + e2)
-    |   _ -> failwith "semantic error")
+    (match (eval e1,eval e2) with
+    | (Constant e1, Constant e2) -> Constant (e1 + e2)
+    | _ -> failwith "tried to add terms that aren't constants")
   | Subtraction (e1, e2) ->
-      (match (eval e1,eval e2) with
-    |   (Constant e1, Constant e2) -> Constant (e1 - e2)
-    |   _ -> failwith "semantic error")
+    (match (eval e1,eval e2) with
+    | (Constant e1, Constant e2) -> Constant (e1 - e2)
+    | _ -> failwith "tried to subtract terms that aren't constants")
   | Multiplication (e1, e2) ->
-      (match (eval e1,eval e2) with
-    |   (Constant e1, Constant e2) -> Constant (e1 * e2)
-    |   _ -> failwith "semantic error")
+    (match (eval e1,eval e2) with
+    | (Constant e1, Constant e2) -> Constant (e1 * e2)
+    | _ -> failwith "tried to multiply terms that aren't constants")
   | Lambda (x, e1) -> Lambda (x, e1)
   | Application (e1, e2) ->
     let e1' = eval e1 in
     let e2' = eval e2 in
     (match e1' with
     | Lambda (x, e) -> eval (subst e x e2')
-    | _ -> failwith "semantic error")
+    | _ -> failwith "tried to apply a term to another that is not a lambda function")
   | IfZero (e1, e2, e3) ->
-      (match eval e1 with
-      | Constant 0 -> eval e2
-      | Constant _ -> eval e3
-      | _ -> failwith "semantic error")
+    (match eval e1 with
+    | Constant 0 -> eval e2
+    | Constant _ -> eval e3
+    | _ -> failwith "the term can't be compared to zero because it's not a constant")
   | Fix e1 ->
     (match eval e1 with
-      | Lambda (x, e) -> eval (subst e x (Fix e1))
-      | _ -> failwith "semantic error")
+    | Lambda (x, e) -> eval (subst e x (Fix e1))
+    | _ -> failwith "tried to apply a term to another that is not a lambda function in a recursive call")
   | Let (x, e1, e2) -> eval (subst e2 x (eval e1))
