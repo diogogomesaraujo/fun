464bffe8759c6a2ee8c47257893b93f3e7a704bd
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-03-16 21:23:09 +0000

    refactoring

--- secd/lib/execute.ml
+++ secd/lib/execute.ml
@@ -2,24 +2,39 @@ open Types
 
 let execute c =
   match c with
-  | (s, e, (LD i)::c, d, m) -> ((List.nth e i)::s, e, c, d, m)
-  | (s, e, (LDC n)::c, d, m) -> ((Int n)::s, e, c, d, m)
-  | ((Int v1)::(Int v2)::s, e, ADD::c, d, m) -> ((Int (v2 + v1))::s, e, c, d, m)
-  | ((Int v1)::(Int v2)::s, e, SUB::c, d, m) -> ((Int (v2 - v1))::s, e, c, d, m)
-  | ((Int v1)::(Int v2)::s, e, MUL::c, d, m) -> ((Int (v2 * v1))::s, e, c, d, m)
+  | (s, e, (LD i)::c, d, m) ->
+    let value = List.nth e i in
+    (value::s, e, c, d, m)
+  | (s, e, (LDC n)::c, d, m) ->
+    let value = Int n in
+    (value::s, e, c, d, m)
+  | ((Int v1)::(Int v2)::s, e, ADD::c, d, m) ->
+    let value = (Int (v2 + v1)) in
+    (value::s, e, c, d, m)
+  | ((Int v1)::(Int v2)::s, e, SUB::c, d, m) ->
+    let value = (Int (v2 - v1)) in
+    (value::s, e, c, d, m)
+  | ((Int v1)::(Int v2)::s, e, MUL::c, d, m) ->
+    let value = (Int (v2 * v1)) in
+    (value::s, e, c, d, m)
   | (s, e, (LDF c')::c, d, m) ->
-    let a = next m in
-    ((Address a)::s, e, c, d, add a (c', e) m)
+    let address = next m in
+    ((Address address)::s, e, c, d, add address (c', e) m)
   | (v::(Address a)::s, e, AP::c, d, m) ->
     let (c', e') = get a m in
     ([], v::e', c', (s, e, c)::d, m)
-  | (v::_, _, RTN::_, (s',e',c')::d, m) -> (v::s', e', c', d, m)
-  | ((Int 0)::s, e, SEL (c1, _)::c, d, m) -> (s, e, c1, ([], [], c)::d, m)
-  | ((Int _)::s, e, SEL (_, c2)::c, d, m) -> (s, e, c2, ([], [], c)::d, m)
-  | (s, e, JOIN::_, (_, _, c')::d, m) -> (s, e, c', d, m)
-  | (s, e, (LDFR c'):: c, d, m) ->
-    let a = next m in
-    ((Address a)::s, e, c, d, add a (c', (Address a)::e) m)
+  | (v::_, _, RTN::_, (s',e',c')::d, m) ->
+    (v::s', e', c', d, m)
+  | ((Int 0)::s, e, SEL (c1, _)::c, d, m) ->
+    (s, e, c1, ([], [], c)::d, m)
+  | ((Int _)::s, e, SEL (_, c2)::c, d, m) ->
+    (s, e, c2, ([], [], c)::d, m)
+  | (s, e, JOIN::_, (_, _, c')::d, m) ->
+    (s, e, c', d, m)
+  | (s, e, (LDFR c')::c, d, m) ->
+    let address = next m in
+    let new_m = add address (c', (Address address)::e) m in
+    ((Address address)::s, e, c, d, new_m)
   | _ -> failwith "error"
 
 let rec execute_t c =
