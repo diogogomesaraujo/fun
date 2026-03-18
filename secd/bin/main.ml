139215ea38ff95024b5af3846eb83fa08f4cce10
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-03-18 18:32:13 +0000

    fib and fact examples

--- secd/bin/main.ml
+++ secd/bin/main.ml
@@ -3,7 +3,7 @@ open Fun.Run
 open Fun.Parse
 
 let () =
-  let fact = parse_from_file "examples/fact.lambda" |> Option.get in
-  match compile fact [] |> run with
+  let fib = parse_from_file "examples/fib.fn" |> Option.get in
+  match compile fib [] |> run with
   | Int x -> Printf.printf "%d\n" x
   | _ -> failwith "Couldn't reach an integer value."
