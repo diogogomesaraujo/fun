e99deffc4b302c67549b6f6b65fe7c02f647840f
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-03-26 13:41:40 +0000

    tracing the max dump and stack sizes

--- fun-secd-compiler/lib/trace.ml
+++ fun-secd-compiler/lib/trace.ml
@@ -5,13 +5,18 @@ open Secd
 
 (** [trace_config c] that constructs a list of the configurations in each step of the execution.*)
 let trace_config c =
-  let rec trace_rec c l =
+  let rec trace_rec c l s_size d_size =
     match execute c with
-    | (_::[], _, [], _, _) -> l
+    | (_::[], _, [], _, _) -> (s_size, d_size, l)
     | c' ->
-      trace_rec (execute c) (l @ [c'])
+      let (s, _, _, d, _) = c' in
+      trace_rec
+        (execute c)
+        (l @ [c'])
+        (max (List.length s) s_size)
+        (max (List.length d) d_size)
   in
-  trace_rec c []
+  trace_rec c [] 0 0
 
 (** [trace i] receives an instruction list [i] and calls [trace_config] for an initial configuration.*)
 let trace i =
